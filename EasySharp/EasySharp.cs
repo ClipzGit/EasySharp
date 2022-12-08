cb738e1608d76ce076911682424eb74c098e5f26
ClipzGit <cameron-straw@outlook.com>
2022-12-08 10:52:49 +0000

    Add project files.

---
+++ EasySharp/EasySharp.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace EasySharp
+{
+    public class EasySharp
+    {
+        // Set Console Title or Loop it
+        public static async Task Title(string title = "Console Window", bool loop_titles = false, string title_one = "", string title_two = "", string title_three = "")
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            if (loop_titles == true)
+            {
+                string[] titles = { title, title_one, title_two, title_three };
+
+                while (true)
+                {
+                    for (int i = 0; i < titles.Length; i++)
+                    {
+                        Console.Title = titles[i];
+                        await Task.Delay(2000);
+                    }
+                }
+                sw.Stop();
+            }
+            else if (loop_titles == false)
+            {
+                Console.Title = title;
+            }
+        
+        }
+        
+        // Write basic line or 'true' for underlined effect
+        public static void WriteLine(string text, bool underlined = false, char underline_icon = '=')
+        {
+            if (underlined == true)
+            {
+                Console.WriteLine(text);
+                foreach (char letter in text)
+                {
+                    Console.Write(underline_icon);
+                }
+                Console.WriteLine();
+            }
+            else if (underlined == false)
+            {
+                Console.WriteLine(text);
+            }
+        }
+
+        // Write a colored line with text color and option for underlined effect
+        public static void WriteColoredLine(string text, ConsoleColor color, bool underlined = false, char underline_icon = '=')
+        {
+            if (underlined == true)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+                foreach (char letter in text)
+                {
+                    Console.Write(underline_icon);
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+            else if (underlined == false)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+        }
+    
+        // Write a colored line with background color and option for underlined effect
+        public static void WriteBackgroundLine(string text, ConsoleColor color, bool underlined = false, char underline_icon = '=')
+        {
+            if (underlined == true)
+            {
+                Console.BackgroundColor = color;
+                Console.WriteLine(text);
+                foreach (char letter in text)
+                {
+                    Console.Write(underline_icon);
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+            else if (underlined == false)
+            {
+                Console.BackgroundColor = color;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+        }
+
+        // Write a custom line with text color, background color, underlined effect
+        public static void WriteCustomLine(string text, ConsoleColor text_color, ConsoleColor bg_color, bool underlined = false, char underline_icon = '=')
+        {
+            if (underlined == true)
+            {
+                Console.ForegroundColor = text_color;
+                Console.BackgroundColor = bg_color;
+                Console.WriteLine(text);
+                foreach (char letter in text)
+                {
+                    Console.Write(underline_icon);
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+            else if (underlined == false)
+            {
+                Console.ForegroundColor = text_color;
+                Console.BackgroundColor = bg_color;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+        }
+
+        // Write a rotating line | NOT USABLE ATM
+        public static async Task RotatingLine(string[] rotating_lines)
+        {
+           for (int i = 0; i < rotating_lines.Length; i++)
+             {
+                Console.Write("\r{0}%   ", rotating_lines[i]);
+                await Task.Delay(2000);
+                    
+             }
+          
+        }
+    }
+}
